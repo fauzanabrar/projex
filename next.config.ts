7228a93a7c42ff72d2616787665ac9d340b2dd73
fauzan.abrar24 <fauzan.abrar24@gmail.com>
2025-03-24 13:41:49 +0000

    Initialize commit

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  allowedDevOrigins: ['3001-idx-projex-1742822060217.cluster-a3grjzek65cxex762e4mwrzl46.cloudworkstations.dev']
+};
+
+export default nextConfig;
