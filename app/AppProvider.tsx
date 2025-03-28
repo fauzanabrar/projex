9712650589a19752939f5a29b04a5b00f293b4a7
fauzanabrar <fauzanabrar24@gmail.com>
2025-03-28 08:23:01 +0800

    add header component

--- app/AppProvider.tsx
+++ app/AppProvider.tsx
@@ -4,13 +4,19 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "sonner";
 import ThemeProvider from "@/components/ThemeProvider";
+import Header from "@/components/Header";
 
 const queryClient = new QueryClient();
 
 const AppProvider = ({ children }: PropsWithChildren) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider>{children}</ThemeProvider>
+      <ThemeProvider>
+        <div className="h-16">
+          <Header />
+        </div>
+        {children}
+      </ThemeProvider>
       <Toaster />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
